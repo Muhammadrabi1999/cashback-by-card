13c8daaffa6b5a7da2606f9b2b4d46d0a4f4ce06
Muhammadrabi1999 <muhammadrabi.shafizoda@bk.ru>
2020-01-11 17:54:57 +0500

    basic realization

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+const REGULAR = "regular";
+const INCREASED = "increased";
+const SPECIAL = "special";
+const prosent1= 0.01;
+const prosent5 = 0.05;
+const prosent30 =0.3;
+ 
+const obj1 = {
+    sum:1000,
+    category:REGULAR
+}
+const obj2 = {
+    sum:1000,
+    category:INCREASED
+}
+const obj3 = {
+    sum:1000,
+    category:SPECIAL
+}
+
+let cashback = (obj1.sum * prosent1) + (obj2.sum * prosent5) + (obj3.sum * prosent30);
+console.log(cashback);
+
