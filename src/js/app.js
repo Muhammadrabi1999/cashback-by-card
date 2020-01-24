dcacd4fe900d1fcdb5eca8758735cda2d1edbbe7
Muhammadrabi1999 <muhammadrabi.shafizoda@bk.ru>
2020-01-24 16:13:02 +0500

    fixes #6

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,18 @@
-
 const regularPurchase = 500;
 const regularPurchasePercent = 0.01;
 
-const increasedPurchase = 600;
+const increasedPurchase = 400;
 const increasedPurchasePercent = 0.05;
 
-const specialPurchase = 700;
+const specialPurchase = 100;
 const scecialPurchasePercent = 0.3;
 
-
-
 const cashbackLimit = 3000;
-
 let cashback = regularPurchase * regularPurchasePercent + 
-increasedPurchase * increasedPurchasePercent + 
-specialPurchase * scecialPurchasePercent;
+    increasedPurchase * increasedPurchasePercent + 
+    specialPurchase * scecialPurchasePercent;
 
     if (cashback > cashbackLimit) {
     cashback = cashbackLimit
 }
 console.log(cashback);
-
